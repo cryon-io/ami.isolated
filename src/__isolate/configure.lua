f46a99423029dcff8f0e4f120fd3cddd77479bf0
cryi <cryi@tutanota.com>
2021-08-05 21:09:05 +0200

    added support for UNPRIVILEGED_PORT_START configuration

--- src/__isolate/configure.lua
+++ src/__isolate/configure.lua
@@ -11,6 +11,21 @@ else
     log_info("User " .. _user .. " found.")
 end
 
+local _unprivilegedPortStart = am.app.get_configuration("UNPRIVILEGED_PORT_START")
+if _user ~= "root" and type(_unprivilegedPortStart) == "string" or type(_unprivilegedPortStart) == "number" then 
+	_trace("Non root user with custom UNPRIVILEGED_PORT_START. Setting net.ipv4.ip_unprivileged_port_start...")
+	local _sysctl = am.plugin.get("sysctl")
+	local _ok, _port = _sysctl.safe_get("net.ipv4.ip_unprivileged_port_start")
+	ami_assert(_ok, "Failed to determine net.ipv4.ip_unprivileged_port_start. Can not configure UNPRIVILEGED_PORT_START!")
+	if tonumber(_port) >= tonumber(_unprivilegedPortStart) then 
+		log_info("UNPRIVILEGED_PORT_START (net.ipv4.ip_unprivileged_port_start) already configured to required value.")
+	else
+		local _ok = _sysctl.safe_set("net.ipv4.ip_unprivileged_port_start", _unprivilegedPortStart)
+		ami_assert(_ok, "Failed to set net.ipv4.ip_unprivileged_port_start to " .. _unprivilegedPortStart .. "!")
+		log_success("UNPRIVILEGED_PORT_START (net.ipv4.ip_unprivileged_port_start) set configured to required value.")
+	end
+end
+
 local _containersConfig = "/home/".._user.."/.config/containers/containers.conf"
 if _user ~= "root" and not fs.exists(_containersConfig) then 
    log_trace("Containers configuration not found. Default configuration will be added into " .. _containersConfig .. ".")
@@ -70,7 +85,6 @@ ami_assert(_ok, "Failed to write isolated app.hjson - " .. (_error or "") .. "!"
 
 local _ok, _error = fs.safe_chown(am.app.get_model("DATA_DIR"), _uid, _uid, { recurse = true })
 assert(_ok, "Failed to chown data directory - " .. (_error or "") .. "!")
-
 
 _podman.raw_exec("rm -f " .. _appId .. "_tmp", { runas = _user })
 local _ok = _podman.run(_imageId, "ami setup", { runas = _user, container = _appId .. "_tmp", args = "-it --mount 'type=bind,src=" .. am.app.get_model("DATA_DIR") .. ",target=/app'", useOsExec = true })
